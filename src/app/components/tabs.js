ffd0777cd834cf7ced3c43b94543ceeaa7989281
Francesco <fdenaro96@gmail.com>
2018-02-02 10:12:15 +0100

    Apply requested changes, improving syntax and element selection

--- src/app/components/tabs.js
+++ src/app/components/tabs.js
@@ -1,37 +1,37 @@
 export default function tabs(container) {
     const active = 'active';
-    const tabs = container.querySelectorAll('.tabs__button');
-    let tabActive = container.querySelector('.js-tabs .active');
-    const contents = [...container.querySelectorAll('.js-content')];
+    const tabs = [...container.querySelectorAll('[data-tabs-tab]')];
+    const contents = [...container.querySelectorAll('[data-tabs-content]')];
+    let tabActive = tabs.find(tab => tab.classList.contains(active));
     let contentActive = contents.find(content => content.classList.contains(active));
-
-    Array.from(tabs, tab =>
+    
+    tabs.forEach((tab) =>
         tab.addEventListener('click', (e) => {
+            const content = contents.find(content => content.dataset.tabsContent === tab.dataset.tabsTab);
             e.preventDefault();
             if (tabActive) {
                 if (tab === tabActive) {
-                    toggleContent(tab, false);
+                    toggleContent(tab);
                 } else {
-                    toggleContent(tabActive, false);
-                    toggleContent(tab, true);
+                    toggleContent(tabActive);
+                    toggleContent(tab, content, true);
                 }
             } else {
-                toggleContent(tab, true);
+                toggleContent(tab, content, true);
             }
         })
     );
 
-    function toggleContent(tab, show) {
-        const contentRef = contents.find(content => content.dataset.tabsContent === tab.dataset.tabsTab);
+    function toggleContent(tab, content, show) {
         if (show) {
-            tab.classList.add(active);
+            tab.classList.toggle(active);
+            content.classList.toggle(active);
             tabActive = tab;
-            contentRef.classList.add(active);
-            contentActive = contentRef;
+            contentActive = content;
         } else {
-            tabActive.classList.remove(active);
+            tabActive.classList.toggle(active);
+            contentActive.classList.toggle(active);
             tabActive = null;
-            contentActive.classList.remove(active);
             contentActive = null;
         }
     }
